e1bc1fa6b5b06e0478f7a27d65db0a8be2014021
Martin Jespersen <mjatzbox@gmail.com>
2016-06-26 16:56:01 +0200

    more content on cases + minor css changes to projects modal

--- js/app.js
+++ js/app.js
@@ -9,47 +9,14 @@ var app = angular.module('portfolio', [
     // imported Dependencies - - - - 
     'ngAnimate',
     'ui.bootstrap',
-    // 'duScroll',
-    // 'mgcrea.jquery',
-    // 'mgcrea.bootstrap.affix',
     'ui.router',
-    // 'elif', // https://github.com/zachsnow/ng-elif - Provides else if to template
-    // 'LocalStorageModule', //angular-local-storage
     'ngSanitize',
-    //'vcRecaptcha',
-    //'angular.vertilize',
-    //'swipe',
+ 
 
-    
-    // Applications - - - - - - - - - 
-
-    //** MENU ***//***
-    // 'MenuService',
-    // 'menuController',
-
-    //** FAQ ***//***
-    // 'FaqService',
-    // 'faqController',
-
-    //** Portfolio ***//***
+    //** Portfolio ***//
     // Controllers
     'MainController',
     
-    //Services
-    //'BeregnerService',
-    
-    // Factories
-    //'BeregnerBasketFactory', //Experimental
-
-    //Directivs
-    //'directives',
-
-    //Animations
-    //'animations',
-
-    // Helpers
-    //'HelperService'
-
 ]).config(function($httpProvider) {
    
 
