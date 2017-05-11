9b734ea781c8365690dbe3aee4b1f3392ff55173
Martin Jespersen <mjatzbox@gmail.com>
2017-05-12 01:18:45 +0200

    fasdffasd

--- js/app.js
+++ js/app.js
@@ -25,52 +25,7 @@ var app = angular.module('portfolio', [
    // each route will pull in a different controller
  
 
-   $routeProvider
-
-       // home page
-       .when('/scotch', {
-           templateUrl: 'page-home.html',
-           controller: 'mainController'
-       })
-
-       // about page
-       .when('/scoio', {
-           templateUrl: 'page-about.html',
-           controller: 'aboutController'
-       })
-
-       // contact page
-       .when('/contact', {
-           templateUrl: 'page-contact.html',
-           controller: 'contactController'
-       });
-
-     // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/hello");
-  
-    // Now set up the states
-    $stateProvider
-
-    .state('hello', {
-        url: "/hello",
-        templateUrl: "partials/hello.html"
-    })
-    .state('projects', {
-        url: "/projects",
-        templateUrl: "partials/projects.html"
-    })
-    .state('projects.list', {
-        url: "/list",
-        templateUrl: "partials/projects.list.html",
-    })
-    .state('about', {
-        url: "/about",
-        templateUrl: "partials/about.html"
-    })
-    .state('tech', {
-        url: "/technologies",
-        templateUrl: "partials/tech.html"
-    })
+   
 
  
 
