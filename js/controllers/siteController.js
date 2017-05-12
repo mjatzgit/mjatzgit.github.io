63b579c75e48be04eb951200241169f16a1bb78c
Martin Jespersen <mjatzbox@gmail.com>
2017-05-12 02:58:41 +0200

    asfvv

--- js/controllers/siteController.js
+++ js/controllers/siteController.js
@@ -4,6 +4,27 @@
  **********************************************************************/
  angular.module('portfolio')
 
+ .controller('ProjectModalCtrl', function ($scope, $uibModalInstance, project) {
+
+    $scope.project = project;
+
+    $scope.close = function () {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+})
+
+ .service('ProjectService', function($http) {
+    return {
+        get : function() {
+            return $http.get('/data/projects.json');
+        }
+    }
+})
+
  .controller('siteController', function ( $window, $scope, ProjectService, $uibModal, $log ) {
 
  	console.log('siteController');
